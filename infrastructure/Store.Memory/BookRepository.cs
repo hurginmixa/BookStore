228a9e05e087fc59c5cd90bc9060a934759f38c0
Mixa Hurgin <mixah@camtek.com>
2020-10-22 22:32:06 +0300

    Занятие 1, Добавить реализацию репозитория в памяти

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"), 
+            new Book(2, "Refactoring"), 
+            new Book(3, "C programming Language"), 
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
